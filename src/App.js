f5c68caa88cee1ae402ae0551f33e0ad205aa99b
Yato <yatoakii@gmail.com>
2022-10-25 23:06:14 +0630

    Added firebase and discord icon

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import React from "react"
+import discordLogo from "./images/discord logo.png"
 
 const App = () => {
+  
+
   return (
-    <div className="App bg-slate-800 h-screen space-x-5">
-      <button className="px-6 py-3 text-sm bg-sky-400 text-white rounded-lg font-semibold">Login</button>
-      <button className="px-6 py-3 text-sm bg-sky-400 text-white rounded-lg font-semibold">Logout</button>
+    <div className="flex text-white bg-slate-800 h-screen space-x-5">
+      <div className="bg-black self-stretch px-3 py-5 flex flex-col items-center">
+        <button className="bg-slate-800 px-4 py-4 rounded-3xl cursor-pointer transition-all ease-in-out delay-50 focus:rounded-2xl focus:bg-blue-600 hover:rounded-2xl hover:bg-blue-600 ">
+          <img 
+            src={discordLogo} 
+            alt="discord logo" 
+            className=" w-7"/>
+        </button>
+        
+      </div>
+      <div>
+
+      </div>
+      <div>
+
+      </div>
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getFirestore} from "@firebase/firestore"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA5zYkZgxIsjsRIq6YPwHttiet8GKC9X1A",
+  authDomain: "discord-clone-e0d12.firebaseapp.com",
+  projectId: "discord-clone-e0d12",
+  storageBucket: "discord-clone-e0d12.appspot.com",
+  messagingSenderId: "763609022916",
+  appId: "1:763609022916:web:1c24a81fa86cf0d3216491"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const firestore = getFirestore(app)
