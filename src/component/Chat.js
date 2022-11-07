aee3044d9e5f9fbffa4a5c4e2d76f6f9c4d01619
Yato <yatoakii@gmail.com>
2022-11-07 10:36:07 +0630

    Can show date and time

--- src/component/Chat.js
+++ src/component/Chat.js
@@ -46,12 +46,27 @@ const Chat = ({server}) => {
                 {
                     message.map((msg) => {
                         return(
-                        <div className='px-7 py-1 space-y-1 my-1 hover:bg-[#1d2738] cursor-pointer'>
+                        <div className='px-7 py-1 space-y-1 my-1 hover:bg-[#1d2738] cursor-pointer' id={msg.id}>
                             <div className='flex gap-3 align-baseline'>
                                 <h2 className='text-white font-semibold'>Kaung Nay Lin Khant</h2>
-                                <h3 className='text-[0.6rem]'>Today at 10:00 AM</h3>
+                                <h3 className='text-[0.6rem]'>{
+                                    (() => {
+                                        const today = new Date().toLocaleDateString()
+                                        const yesterday = new Date(new Date().setDate(new Date().getDate()-1)).toLocaleDateString()
+                                        const createdDate = new Date(msg.data.created.seconds*1000).toLocaleDateString()
+                                        if (today === createdDate){
+                                            return "Today " + new Intl.DateTimeFormat('en-US', {hour: '2-digit', minute: '2-digit'}).format(msg.data.created.seconds*1000)
+                                        }else if(yesterday === createdDate ){
+                                            return "Yesterday  " + new Intl.DateTimeFormat('en-US', {hour: '2-digit', minute: '2-digit'}).format(msg.data.created.seconds*1000)
+                                        }
+                                        else{
+                                            return new Intl.DateTimeFormat('en-US', {year: 'numeric', month: '2-digit',day: '2-digit', hour: '2-digit', minute: '2-digit'}).format(msg.data.created.seconds*1000)
+                                        }
+                                    })()
+                                }
+                                </h3>
                             </div>
-                            <p id={msg.id}>{msg.data.text}</p>
+                            <p>{msg.data.text}</p>
                         </div>
                         )
                     })
