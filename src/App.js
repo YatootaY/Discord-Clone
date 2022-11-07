816724f70fe63848145b2280ab3001581f9ce380
Yato <yatoakii@gmail.com>
2022-11-07 14:02:39 +0630

    Sign in appear

--- src/App.js
+++ src/App.js
@@ -1,19 +1,76 @@
-import React,{useState} from "react"
+import React,{useState, useEffect} from "react"
 import Nav from "./component/Nav.js"
 import Chat from "./component/Chat.js"
+import {
+  getAuth,
+  onAuthStateChanged,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut,
+} from '@firebase/auth';
 
 const App = () => {
-
   const [server,setServer] = useState("main")
   
   const serverClicked = (serverName) => {
-      setServer(serverName)
+    setServer(serverName)
+  }
+  const signIn = () => {
+    const provider = new GoogleAuthProvider()
+    signInWithPopup(getAuth(),provider)
   }
 
+  const signOutUser = () => {
+    signOut(getAuth());
+  }
+
+  
+  const getProfilePicUrl = () => {
+    return getAuth().currentUser.photoURL || './images/discord logo.png';
+  }
+  
+  const getUserName = () => {
+    return getAuth().currentUser.displayName;
+  }
+
+
+  const authStateObserver = () => {
+    const signInBtn = document.getElementById("signIn")
+    const signOutBtn = document.getElementById("signOut")
+    const userNameEle = document.getElementById("username")
+    if (isUserSignedIn()) { 
+      const userName = getUserName();
+      userNameEle.textContent = userName;
+  
+      userNameEle.removeAttribute('hidden');
+      signOutBtn.removeAttribute('hidden');
+
+      signInBtn.setAttribute('hidden', 'true');
+    } else { 
+      userNameEle.setAttribute('hidden', 'true');
+      signOutBtn.setAttribute('hidden', 'true');
+      signInBtn.removeAttribute('hidden');
+    }
+  }
+
+  const initFirebaseAuth = () => {
+
+    onAuthStateChanged(getAuth(), authStateObserver);
+  }
+
+  const isUserSignedIn = () => {
+    return !!getAuth().currentUser;
+  }
+
+  useEffect( () => {
+    initFirebaseAuth()
+  },[])
+  
+  
   return (
     <div className="flex text-white bg-slate-800 h-screen">
       <Nav serverClicked={serverClicked}/>
-      <Chat server={server}/>
+      <Chat server={server} signIn={signIn} signOutUser={signOutUser}/>
     </div>
   );
 }
--- src/component/Nav.js
+++ src/component/Nav.js
@@ -3,10 +3,17 @@ import discordLogo from "../images/discord logo.png"
 import onePiece from "../images/one piece.jpg"
 import firebase from "../images/firebase.png"
 
-const Nav = ({serverClicked}) => {
+const Nav = ({serverClicked,signIn,signOutUser}) => {
 
+    console.log(signIn)
+    console.log(signOutUser)
     return(
         <div className="bg-black self-stretch px-5 py-5 flex flex-col items-center w-fit gap-7">
+        <div id="authArea">
+            <h1 id="username" hidden="true"></h1>
+            <button id="signIn" hidden="true" onClick={signIn}>Sign in</button>
+            <button id="signOut" hidden="true" onClick={signOutUser}>Sign Out</button>
+        </div>
             <div className="pb-5 border-b border-slate-800 border-lengt flex flex-col gap-3 items-center">
                 <h1 className="font-semibold text-sm text-slate-400">Discord</h1>
                 <button onClick={() => serverClicked("main")} className="bg-slate-800 px-4 py-4 rounded-3xl cursor-pointer transition-all ease-in-out delay-50 focus:rounded-2xl focus:bg-blue-600 hover:rounded-2xl hover:bg-blue-600 ">
@@ -17,9 +24,9 @@ const Nav = ({serverClicked}) => {
                 </button>
             </div>
             
-            <img src={onePiece} alt="one" onClick={() => serverClicked("onePiece")} className=" transition-all duration-400 ease-in-out object-cover w-14 h-14 bg-slate-800 rounded-[50%] cursor-pointer hover:rounded-2xl "/>
+            <img src={onePiece} alt="one" onClick={() => serverClicked("onePiece")} className=" transition-all duration-600 ease-in-out object-cover w-14 h-14 bg-slate-800 rounded-[50%] cursor-pointer hover:rounded-2xl "/>
 
-            <img src={firebase} alt="one" onClick={() => serverClicked("firebase")} className=" transition-all duration-400 ease-in-out object-cover w-14 h-14 bg-slate-800 rounded-[50%] cursor-pointer hover:rounded-2xl "/>
+            <img src={firebase} alt="one" onClick={() => serverClicked("firebase")} className=" transition-all duration-600 ease-in-out object-cover w-14 h-14 bg-slate-800 rounded-[50%] cursor-pointer hover:rounded-2xl "/>
         </div>
     )
 }
