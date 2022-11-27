374c89537c4192597560421c883a6f5c87577241
Yato <yatoakii@gmail.com>
2022-11-27 13:10:47 +0630

    Can view uploaded photos as messages

--- src/component/component/Messages.js
+++ src/component/component/Messages.js
@@ -7,29 +7,39 @@ const Messages = ({message}) => {
                 {
                     message.map((msg) => {
                         return(
-                        <div className='px-7 py-1 space-y-1 my-1 hover:bg-[#1d2738] cursor-pointer flex gap-3 items-center' key={msg.id}>
-                            <img src={msg.data.profileUrl} alt="profilePic" className='w-10 h-10 rounded-full bg-center bg-cover'/>
+                        <div className='px-7 py-1 space-y-1 my-1 hover:bg-[#1d2738] cursor-pointer flex gap-3 items-top' key={msg.id}>
+                            <img src={msg.data.profileUrl} alt="profilePic" className='w-10 h-10 mt-2 rounded-full bg-center bg-cover'/>
                             <div className="flex gap-1 flex-col">
-                            <div className='flex gap-3 align-baseline'>
-                                <h2 className='text-white font-semibold md:text-base text-sm'>{msg.data.userName}</h2>
-                                <h3 className='text-[0.6rem]'>{
-                                    (() => {
-                                        const today = new Date().toLocaleDateString()
-                                        const yesterday = new Date(new Date().setDate(new Date().getDate()-1)).toLocaleDateString()
-                                        const createdDate = new Date(msg.data.created.seconds*1000).toLocaleDateString()
-                                        if (today === createdDate){
-                                            return "Today " + new Intl.DateTimeFormat('en-US', {hour: '2-digit', minute: '2-digit'}).format(msg.data.created.seconds*1000)
-                                        }else if(yesterday === createdDate ){
-                                            return "Yesterday  " + new Intl.DateTimeFormat('en-US', {hour: '2-digit', minute: '2-digit'}).format(msg.data.created.seconds*1000)
-                                        }
-                                        else{
-                                            return new Intl.DateTimeFormat('en-US', {year: 'numeric', month: '2-digit',day: '2-digit', hour: '2-digit', minute: '2-digit'}).format(msg.data.created.seconds*1000)
-                                        }
-                                    })()
-                                }
-                                </h3>
-                            </div>
-                            <p className="md:text-base text-sm">{msg.data.text}</p>
+                                <div className='flex gap-3 align-baseline'>
+                                    <h2 className='text-white font-semibold md:text-base text-sm'>{msg.data.userName}</h2>
+                                    <h3 className='text-[0.6rem]'>{
+                                        (() => {
+                                            const today = new Date().toLocaleDateString()
+                                            const yesterday = new Date(new Date().setDate(new Date().getDate()-1)).toLocaleDateString()
+                                            const createdDate = new Date(msg.data.created.seconds*1000).toLocaleDateString()
+                                            if (today === createdDate){
+                                                return "Today " + new Intl.DateTimeFormat('en-US', {hour: '2-digit', minute: '2-digit'}).format(msg.data.created.seconds*1000)
+                                            }else if(yesterday === createdDate ){
+                                                return "Yesterday  " + new Intl.DateTimeFormat('en-US', {hour: '2-digit', minute: '2-digit'}).format(msg.data.created.seconds*1000)
+                                            }
+                                            else{
+                                                return new Intl.DateTimeFormat('en-US', {year: 'numeric', month: '2-digit',day: '2-digit', hour: '2-digit', minute: '2-digit'}).format(msg.data.created.seconds*1000)
+                                            }
+                                        })()
+                                    }
+                                    </h3>
+                                </div>
+                                <div>
+                                    {
+                                        (() => {
+                                            if(msg.data.imageUrl){
+                                                return(<img src={msg.data.imageUrl} className="md:w-[65%] w-[80%]"/>)
+                                            }else{
+                                                return(<p className="md:text-base text-sm">{msg.data.text}</p>);
+                                            }
+                                        })()
+                                    }
+                                </div>
                             </div>
                         </div>
                         )
