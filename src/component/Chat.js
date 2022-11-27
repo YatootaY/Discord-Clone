935cbe13611b2ffacae0415390af6376a354fee7
Yato <yatoakii@gmail.com>
2022-11-27 11:32:10 +0630

    Added file upload interface

--- src/component/Chat.js
+++ src/component/Chat.js
@@ -54,10 +54,18 @@ const Chat = ({server,signInClicked,signOutClicked,isUserSignedIn,userName,profi
                 <LoginArea signInClicked={signInClicked} signOutClicked={signOutClicked}/>
             </div>
             <Messages message={message}/>
-            <form onSubmit={handleSave} className="m-4 px-3 py-1 gap-5 flex items-center bg-slate-600 rounded-lg">
-                <input className="focus:outline-none bg-slate-600 p-2 ml-3 flex-grow" id="msg" type="text" placeholder='Enter your message' ref={messageRef} autoComplete="off"/>
-                <svg className="h-6 w-6 mr-3 text-slate-400 hover:text-slate-200 transition-all duration-200 cursor-pointer"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round">  <rect x="3" y="3" width="18" height="18" rx="2" ry="2" />  <circle cx="8.5" cy="8.5" r="1.5" />  <polyline points="21 15 16 10 5 21" /></svg>
-            </form>
+            <div className="m-4 px-3 py-1 gap-5 flex items-center bg-slate-600 rounded-lg">
+                <form onSubmit={handleSave} className="grow">
+                    <input className="w-full focus:outline-none bg-slate-600 p-2 ml-3 flex-grow" id="msg" type="text" placeholder='Enter your message' ref={messageRef} autoComplete="off"/>
+                </form>
+                <form id="image-form" action="#">
+                    <label>
+                    <input id="mediaCapture" type="file" accept="image/*" capture="camera" hidden={true}/>
+                    <svg className="h-6 w-6 mr-3 text-slate-400 hover:text-slate-200 transition-all duration-200 cursor-pointer"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round">  <rect x="3" y="3" width="18" height="18" rx="2" ry="2" />  <circle cx="8.5" cy="8.5" r="1.5" />  <polyline points="21 15 16 10 5 21" /></svg>
+                    </label>
+                </form>
+            </div>
+            
         </div>
     )
 }
