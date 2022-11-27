316f06a980d46f2090cd2340b446de8413e94d18
Yato <yatoakii@gmail.com>
2022-11-27 16:14:37 +0630

    Kinda working

--- src/component/component/LoginArea.js
+++ src/component/component/LoginArea.js
@@ -4,8 +4,8 @@ import React from "react";
 const LoginArea = ({signInClicked,signOutClicked}) => {
 
     return(
-        <div id="authArea" className='flex items-center gap-5'>
-            <img src="" alt="profilePic" className='md:w-14 md:h-14 w-10 h-10 rounded-full bg-center bg-cover m-auto' id="profilePic" hidden={true}/>
+        <div id="authArea" className='flex items-center gap-5 my-2'>
+            <img src="" alt="profilePic" className='md:w-12 md:h-12 w-10 h-10 rounded-full bg-center bg-cover m-auto' id="profilePic" hidden={true}/>
             <div className="flex flex-col justify-center space-y-[-5px]">
                 <p id="username" hidden={true} className='font-semibold md:text-sm text-xs'></p>
                 <a id="signOut" hidden={true} onClick={signOutClicked} className="cursor-pointer hover:underline transition-all duration-300 md:text-[0.6rem] text-[0.5rem] font-semibold ">Log out</a>
--- src/component/component/Messages.js
+++ src/component/component/Messages.js
@@ -1,45 +1,76 @@
-import React from "react";
+import React,{useRef} from "react";
 
 
 const Messages = ({message}) => {
+    const prevMsg = useRef()
+    const continuous = useRef()
+    const isMessageContinuous = (newMsg,prev) => {
+        if (!prev.current){
+            prevMsg.current = newMsg
+            continuous.current = false;
+            return;
+        }
+        const minutePassed = (newMsg.data.created.seconds-prev.current.data.created.seconds)/60
+        console.log(newMsg.data.text,prevMsg.current.data.text)
+        continuous.current = (newMsg.data.uid === prev.current.data.uid && minutePassed <= 1)
+        prevMsg.current = newMsg
+        
+        return;
+    }
     return(
-        <div className='py-7 flex-grow flex flex-col gap-1 overflow-auto ' id='chatarea'>
+        <div className='py-7 flex-grow flex flex-col overflow-auto ' id='chatarea'>
                 {
                     message.map((msg) => {
+                        isMessageContinuous(msg,prevMsg)
                         return(
-                        <div className='px-7 py-1 space-y-1 my-1 hover:bg-[#1d2738] cursor-pointer flex gap-3 items-top' key={msg.id}>
-                            <img src={msg.data.profileUrl} alt="profilePic" className='w-10 h-10 mt-2 rounded-full bg-center bg-cover'/>
-                            <div className="flex gap-1 flex-col">
-                                <div className='flex gap-3 align-baseline'>
-                                    <h2 className='text-white font-semibold md:text-base text-sm'>{msg.data.userName}</h2>
-                                    <h3 className='text-[0.6rem]'>{
-                                        (() => {
-                                            const today = new Date().toLocaleDateString()
-                                            const yesterday = new Date(new Date().setDate(new Date().getDate()-1)).toLocaleDateString()
-                                            const createdDate = new Date(msg.data.created.seconds*1000).toLocaleDateString()
-                                            if (today === createdDate){
-                                                return "Today " + new Intl.DateTimeFormat('en-US', {hour: '2-digit', minute: '2-digit'}).format(msg.data.created.seconds*1000)
-                                            }else if(yesterday === createdDate ){
-                                                return "Yesterday  " + new Intl.DateTimeFormat('en-US', {hour: '2-digit', minute: '2-digit'}).format(msg.data.created.seconds*1000)
-                                            }
-                                            else{
-                                                return new Intl.DateTimeFormat('en-US', {year: 'numeric', month: '2-digit',day: '2-digit', hour: '2-digit', minute: '2-digit'}).format(msg.data.created.seconds*1000)
-                                            }
-                                        })()
+                        <div className='px-7 py-1 hover:bg-[#1d2738] cursor-pointer flex gap-3 items-top' key={msg.id}>
+                            {    
+                                (() => {
+                                    if(!continuous.current){
+                                        return(<img src={msg.data.profileUrl} alt="profilePic" className='w-10 h-10 mt-2 rounded-full bg-center bg-cover'/>)
+                                    }else{
+                                        return(<div className="w-10 h-10"></div>)
                                     }
-                                    </h3>
-                                </div>
-                                <div>
-                                    {
-                                        (() => {
-                                            if(msg.data.imageUrl){
-                                                return(<img src={msg.data.imageUrl} className="md:w-[65%] w-[80%]"/>)
-                                            }else{
-                                                return(<p className="md:text-base text-sm">{msg.data.text}</p>);
-                                            }
-                                        })()
-                                    }
-                                </div>
+                                })()
+                            }
+                            <div className="flex flex-col">
+                                {    
+                                    (() => {
+                                        if(!continuous.current){
+                        
+                                            return(
+                                                <div>
+                                                    <h2 className='text-white font-semibold md:text-base text-sm'>{msg.data.userName}</h2>
+                                                    <h3 className='text-[0.6rem]'>{
+                                                        (() => {
+                                                            const today = new Date().toLocaleDateString()
+                                                            const yesterday = new Date(new Date().setDate(new Date().getDate()-1)).toLocaleDateString()
+                                                            const createdDate = new Date(msg.data.created.seconds*1000).toLocaleDateString()
+                                                            if (today === createdDate){
+                                                                return "Today " + new Intl.DateTimeFormat('en-US', {hour: '2-digit', minute: '2-digit'}).format(msg.data.created.seconds*1000)
+                                                           }else if(yesterday === createdDate ){
+                                                                return "Yesterday  " + new Intl.DateTimeFormat('en-US', {hour: '2-digit', minute: '2-digit'}).format(msg.data.created.seconds*1000)
+                                                            }
+                                                            else{
+                                                                return new Intl.DateTimeFormat('en-US', {year: 'numeric', month: '2-digit',day: '2-digit', hour: '2-digit', minute: '2-digit'}).format(msg.data.created.seconds*1000)
+                                                            }
+                                                        })()
+                                                    }
+                                                    </h3>
+                                                </div>
+                                            )
+                                        }
+                                    })()
+                                }
+                                {
+                                    (() => {
+                                        if(msg.data.imageUrl){
+                                            return(<img src={msg.data.imageUrl} className="w-[65%]"/>)
+                                        }else{
+                                            return(<p className="md:text-base text-sm">{msg.data.text}</p>);
+                                        }
+                                    })()
+                                }
                             </div>
                         </div>
                         )
